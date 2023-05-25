499bf86778694d9cdfe29198980a34dcdfcaae5d
gmergut <gonzaloshadow@hotmail.com>
2023-05-25 12:05:37 +0200

    Subida inicial del test

---
+++ sql/Prueba2.java
@@ -0,0 +1,26 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/classicmodels?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";
+		String usuario = "root";
+		String password = "1234";
+		try (Connection con = DriverManager.getConnection(url, usuario, password);
+				PreparedStatement consulta = con.prepareStatement("Select productName as nombre from products")) {
+
+			ResultSet resultados = consulta.executeQuery();
+			while (resultados.next()) {
+				System.out.println(resultados.getString("nombre"));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
